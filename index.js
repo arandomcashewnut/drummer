63593639519c177041eede9f29f979ac3901d74a
aRandomCashewnut <84876646+arandomcashewnut@users.noreply.github.com>
2021-05-30 02:02:13 +0530

    Update index.js with refactored and commented code

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-
+// Check for mouse click on drum buttons
 drumButtons = document.querySelectorAll('.drum')
+for (i = 0; i < drumButtons.length; i++) {
+    drumButtons[i].addEventListener('click', function () {
+        playSound(this.innerHTML)
+    })
+}
+
+// Check for keyboard key press
+document.addEventListener('keydown', (event) => playSound(event.key))
+
 tom1Sound = new Audio('sounds/tom-1.mp3')
 tom2Sound = new Audio('sounds/tom-2.mp3')
 tom3Sound = new Audio('sounds/tom-3.mp3')
@@ -8,39 +17,8 @@ snareSound = new Audio('sounds/snare.mp3')
 kickSound = new Audio('sounds/kick-bass.mp3')
 crashSound = new Audio('sounds/crash.mp3')
 
-for (i = 0; i < drumButtons.length; i++) {
-    drumButtons[i].addEventListener('click', function () {
-        switch (this.innerHTML) {
-            case 'w':
-                tom1Sound.play()
-                break;
-            case 'a':
-                tom2Sound.play()
-                break;
-            case 's':
-                tom3Sound.play()
-                break;
-            case 'd':
-                tom4Sound.play()
-                break;
-            case 'j':
-                snareSound.play()
-                break;
-            case 'k':
-                crashSound.play()
-                break;
-            case 'l':
-                kickSound.play()
-                break;
-            
-            default: console.log(this.innerHTML)
-            
-        }
-    })
-}
-
-document.addEventListener('keydown', function (event) {
-    switch (event.key) {
+function playSound(key) {
+    switch (key) {
         case 'w':
             tom1Sound.play()
             break;
@@ -62,7 +40,6 @@ document.addEventListener('keydown', function (event) {
         case 'l':
             kickSound.play()
             break;
-        
-        default: console.log(event)
+        default: console.log(key)
     }
-})
+}
